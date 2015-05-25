bd3f70bfeb9a018497829068ae38380e81cd1c42
Cameron Thomas <tho09041@byui.com>
2015-05-25 15:10:50 -0600

    databases stuff

--- signInPage.php
+++ signInPage.php
@@ -6,26 +6,49 @@ $username = 'root';
 $password = 'root';*/
 $userID = 1;
 
-$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
 $dbname = "nba_predictor_app";
+
+if ($openShiftVar === null || $openShiftVar == "")
+{
+     // Not in the openshift environment
+     $dbHost = "localhost";
+     $server = '127.0.0.1';
+     $database = 'nba_predictor_app';
+     $username = 'root';
+     $password = 'root';
+
+     $db = new PDO("mysql: host=$server; dbname=$database", $username, $password);
+     // …
+}
+else
+{
+     // In the openshift environment
+     $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+     $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+     $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+     $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+     $dbname = "nba_predictor_app";
+     $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+}
+
+     $groupID = 2;
+
 
 //$dsn = 'mysql:dbname='.$dbname.';host='.DB_HOST.';port='.DB_PORT;
 
-$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
 
 //$db = new PDO($dsn, DB_USER, DB_PASS);
 
 //$db = new PDO("mysql: host=$server; dbname=$database", $username, $password);
 $users = $db->query('Select username from user;');
 
-foreach($users as $row)
+/*foreach($users as $row)
 {
   echo $row[username] . "<br />";
 }
-
+*/
 ?>
 
 <!DOCTYPE html>
